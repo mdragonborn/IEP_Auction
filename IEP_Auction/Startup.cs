c6d30a86d88b40770eb940636f379fbe33ad6597
mdragonborn <lena.markovic996@gmail.com>
2019-01-06 17:27:06 +0100

    Init

---
+++ IEP_Auction/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IEP_Auction.Startup))]
+namespace IEP_Auction
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
